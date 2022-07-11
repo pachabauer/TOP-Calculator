377ab31ec465858ed874d9cf6a71dc16f6a0e5ec
pachaB <eabestudio@gmail.com>
2022-07-11 17:54:03 -0300

    js display shows numbers

--- code.js
+++ code.js
@@ -1,3 +1,28 @@
-// create variables //
-const body  = document.querySelector('body');
+// functions
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  return num1 / num2;
+}
+
+function operate(funct, num1, num2) {}
+
+let screen = document.getElementById("item1");
+let buttons = document.getElementsByClassName("button");
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].onclick = function () {
+    screen.textContent = screen.textContent + this.textContent;
+    console.log(i);
+  };
+}
